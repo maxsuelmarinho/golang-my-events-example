b252644b70d7baf074b6acea09e6619e0a711379
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-03-07 02:22:15 -0300

    Setting environment variables on gitlab

--- gitlab/setup-gitlab.sh
+++ gitlab/setup-gitlab.sh
@@ -20,52 +20,51 @@ done
 echo "Creating access token..."
 /opt/gitlab/bin/gitlab-rails r "token=PersonalAccessToken.new(user: User.where(id: 1).first, name: 'token teste', token: '$GITLAB_PRIVATE_TOKEN', scopes: Gitlab::Auth::available_scopes); token.save!"
 
-#bash /init-repo.sh $GITLAB_PRIVATE_TOKEN $GITLAB_GROUP_NAME $GITLAB_REPO_NAME $GITLAB_API_PORT
 privateToken=$GITLAB_PRIVATE_TOKEN
 groupName=$GITLAB_GROUP_NAME
 repoName=$GITLAB_REPO_NAME
 apiPort=$GITLAB_API_PORT
 
+userId=""
+groupId=""
+projectId=""
 while true; do
   echo "Waiting http server warms up...";
   httpStatus=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$apiPort/-/health)
-  if [[ $httpStatus == 200 ]]; then
+  if [[ $httpStatus != 200 ]]; then
+    sleep 30s;
+    continue;
+  fi
+
+  keysCount="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/users/$userId/keys" | jq length)";
+  if [[ -z $keysCount || $keysCount != "1" ]]; then
+    echo "Add SSH key to root user";
+    userId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/users?username=root" | jq '.[0].id')";
+    curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST -F "title=at vagrant" -F "key=$(cat /root/.ssh/id_rsa.pub)" "http://localhost:$apiPort/api/v4/users/$userId/keys" | jq;
+  fi
+
+  echo "Verifying if group $groupName already exists...";
+  groupId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/groups?search=$groupName" | jq '.[0].id')"
+  if [[ -z $groupId || $groupId == "null" ]]; then
+    echo "Group not found. Creating '$groupName' group...";
+    curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST "http://localhost:$apiPort/api/v4/groups?name=$groupName&path=$groupName" | jq;
+    groupId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/groups?search=$groupName" | jq '.[0].id')";
+  fi
+
+  echo "Verifying if repository $repoName already exists...";
+  projectId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/projects/?search=$repoName" | jq '.[0].id')";
+  if [[ -z $projectId || $projectId == "null" ]]; then
+    echo "Repository not found. Creating '$repoName' repository...";
+    curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST "http://localhost:$apiPort/api/v4/projects?name=$repoName" | jq;
+    projectId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/projects/?search=$repoName" | jq '.[0].id')";
+  fi
+
+  if [[ ! -z $keysCount && $keysCount == "1" && ! -z $groupId && $groupId != "null" && ! -z $projectId && $projectId != "null" ]]; then
     break;
   fi
+
   sleep 30s;
 done;
-
-echo "Add SSH key to root user"
-userId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/users?username=root" | jq '.[0].id')"
-curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST -F "title=at vagrant" -F "key=$(cat /root/.ssh/id_rsa.pub)" "http://localhost:$apiPort/api/v4/users/$userId/keys" | jq
-
-echo "Verifying if group $groupName already exists..."
-groupId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/groups?search=$groupName" | jq '.[0].id')"
-
-echo "Verifying if repository $repoName already exists..."
-projectId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/projects/?search=$repoName" | jq '.[0].id')"
-
-if [[ $groupId != "null" && $projectId != "null" ]]; then
-  echo "Group '$groupName' and repository '$repoName' already exist. Skiping..."
-fi
-
-# Create a group
-if [[ $groupId == "null" ]]; then
-  echo "Group not found. Creating '$groupName' group..."
-  curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST "http://localhost:$apiPort/api/v4/groups?name=$groupName&path=$groupName" | jq
-  groupId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/groups?search=$groupName" | jq '.[0].id')"
-fi
-
-# Create a repo
-if [[ $projectId == "null" ]]; then
-  echo "Repository not found. Creating '$repoName' repository..."
-  curl -s --header "PRIVATE-TOKEN: $privateToken" -X POST "http://localhost:$apiPort/api/v4/projects?name=$repoName" | jq
-  projectId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" "http://localhost:$apiPort/api/v4/projects/?search=$repoName" | jq '.[0].id')"
-fi
-
-if [[ $groupId == "null" && $projectId == "null" ]]; then
-  echo "Ops! Something went wrong! Group '$groupName' and repository '$repoName' not exist."
-fi
 
 # transfer the repo to the group
 echo "Transfering '$repoName' repository to '$groupName' group..."
@@ -75,9 +74,11 @@ runnersToken="$(curl -s --header "PRIVATE-TOKEN: $privateToken" http://localhost
 
 #runnerId="$(curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/runners" --form "token=$runnersToken" --form "description=docker-runner" --form "tag_list=docker" --form "run_untagged=true" --form "locked=false") | jq -r '.id'"
 
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=DOCKER_USERNAME" --form "value=${DOCKER_USERNAME}" | jq
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=DOCKER_PASSWORD" --form "value=${DOCKER_PASSWORD}" | jq
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_TOKEN" --form "value=${KUBE_TOKEN}" | jq
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_CA_CERT" --form "value=${KUBE_CA_CERT}" | jq
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_SERVER" --form "value=${KUBE_SERVER}" | jq
-curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_CLUSTER" --form "value=${KUBE_CLUSTER}" | jq
+if [[ ! -z $projectId && $projectId != "null" ]]; then
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=DOCKER_USERNAME" --form "value=${DOCKER_USERNAME}" | jq
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=DOCKER_PASSWORD" --form "value=${DOCKER_PASSWORD}" | jq
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_TOKEN" --form "value=${KUBE_TOKEN}" | jq
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_CA_CERT" --form "value=${KUBE_CA_CERT}" | jq
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_SERVER" --form "value=${KUBE_SERVER}" | jq
+  curl -s --header "PRIVATE-TOKEN: $privateToken" --request POST "http://localhost:$apiPort/api/v4/projects/$projectId/variables" --form "key=KUBE_CLUSTER" --form "value=${KUBE_CLUSTER}" | jq
+fi
